9d930765c09d74e1c4bb6c8cfc8e2cc63f423e19
Adam Borek <borekadam89@gmail.com>
2025-01-04 13:34:06 +0100

    Update lab_09

--- lab_09/lbm_logic.py
+++ lab_09/lbm_logic.py
@@ -32,7 +32,6 @@ def stream_optimized(f_out, wall_mask, height, width, velocities, opposite, boun
                 if 0 <= ni < height and 0 <= nj < width and not wall_mask[ni, nj]:
                     f_temp[ni, nj, k] = f_out[i, j, k]
                 else:
-                    #if boundary_condition == "bounce-back":
                     f_temp[i, j, opposite[k]] = f_out[i, j, k]
 
     return f_temp
@@ -67,22 +66,21 @@ class LBM:
         #    self.rho[:, j] = CELL_DENSITY  # Gęstość płynu po lewej stronie
 
         if boundary_condition == "constant":
-            # Górna granica: u_x = 0.02, u_y = 0
+            # Górna granica:
             self.ux[0, :] = 0.02
             self.uy[0, :] = 0.0
-            #self.rho[0, :] = 1.0
 
-            # Dolna granica: u_x = 0, u_y = 0
+            # Dolna granica:
             self.ux[-1, :] = 0.0
             self.uy[-1, :] = 0.0
-            #self.rho[-1, :] = 1.0
 
-            # Lewa i prawa granica: liniowe zmiany prędkości od dołu (0) do góry (0.02)
+            # Lewa i prawa granica:
             for i in range(self.height):
                 self.ux[self.height - i - 1, 0] = 0.02 * (i / (self.height - 1))  # Lewa granica
                 self.ux[self.height - i - 1, -1] = 0.02 * (i / (self.height - 1))  # Prawa granica
                 self.uy[self.height - i - 1, 0] = 0.0
                 self.uy[self.height - i - 1, -1] = 0.0
+
         elif boundary_condition == "custom":
             # Lewa granica: liniowa zmiana prędkości
             for i in range(self.height):
@@ -92,23 +90,10 @@ class LBM:
             # Prawa granica: stała gęstość
             for i in range(self.height):
                 self.rho[i, -1] = 1.0  # Stała gęstość
-                self.ux[i, -1] = 0.0
+                self.ux[i, -1] = (self.f_in[i, -1, 0] + self.f_in[i, -1, 3] + self.f_in[i, -1, 4] + 2 * (
+                           self.f_in[i, -1, 1] + self.f_in[i, -1, 5] + self.f_in[i, -1, 8]))
                 self.uy[i, -1] = 0.0
 
-            # Górna granica: symetryczny warunek
-            #self.ux[0, :] = self.ux[1, :]  # Symetryczne względem środka
-            #self.uy[0, :] = 0.0
-          #  for j in range(self.width):
-           #     u_h = self.ux[1, j]  # Symetria z wierszem poniżej
-           #     self.ux[0, j] = u_h  # Symetryczna prędkość pozioma
-          #      self.uy[0, j] = 0.0  # Prędkość pionowa zerowa
-
-                # Wyliczanie gęstości z równania (16e)
-               # self.rho[0, j] = (
-                 #                        self.f_in[0, j, 0]
-                  #                       + 2 * (self.f_in[0, j, 1] + self.f_in[0, j, 3])
-                   #                      + 4 * (self.f_in[0, j, 5] + self.f_in[0, j, 7])
-                     #            ) / (1 - u_h)
 
         # Wyliczenie wartości równowagi dla każdej komórki
         for i in range(self.height):
@@ -146,68 +131,32 @@ class LBM:
         elif boundary_condition == "custom":
             # Prawo: warunek otwarty z zadaną gęstością
             for i in range(self.height):
-                self.rho[i, -1] = 1.0  # Zdefiniowana gęstość
+                self.rho[i, -1] = 1.0
 
-                self.ux[i, -1] = (self.f_in[i, -1, 0] + self.f_in[i, -1, 3] + self.f_in[i, -1, 4] + 2 * (self.f_in[i, -1, 1] + self.f_in[i, -1, 5] + self.f_in[i, -1, 8])) / self.rho[i, -1] - 1.0
-                # Zakładamy Uy = 0 (prędkość pionowa)
+                self.ux[i, -1] = 1 - (self.f_in[i, -1, 0] + self.f_in[i, -1, 3] + self.f_in[i, -1, 4] + 2 * (self.f_in[i, -1, 1] + self.f_in[i, -1, 5] + self.f_in[i, -1, 8])) / self.rho[i, -1]
+                self.uy[i, -1] = 0.0
 
-                #self.uy[i, -1] = 0.0
-                self.uy[i, -1] = 0.6 * (self.f_in[i, -1, 3] - self.f_in[i, -1, 4] + self.f_in[i, -1, 1] - self.f_in[i, -1, 8]) / self.rho[i, -1] / (5 - 3 * self.ux[i, -1])
+                self.f_in[i, -1, 1] = self.f_in[i, -1, 2] + 2.0 / 3.0 * self.ux[i, -1] * self.rho[i, -1]
+                self.f_in[i, -1, 5] = self.f_in[i, -1, 7] + 1.0 / 6.0 * self.ux[i, -1] * self.rho[i, -1]
+                self.f_in[i, -1, 8] = self.f_in[i, -1, 6] + 1.0 / 6.0 * self.ux[i, -1] * self.rho[i, -1]
 
-                self.f_in[i, -1, 2] = self.f_in[i, -1, 1] - 2.0 / 3.0 * self.ux[i, -1] * self.rho[i, -1]
-                self.f_in[i, -1, 7] = self.f_in[i, -1, 5] - 1.0 / 6.0 * (self.ux[i, -1] - self.uy[i, -1]) * self.rho[i, -1]
-                self.f_in[i, -1, 6] = self.f_in[i, -1, 8] - 1.0 / 6.0 * (self.ux[i, -1] + self.uy[i, -1]) * self.rho[i, -1]
-
-                #self.f_in[i, -1, 1] = self.f_in[i, -1, 2] - 2.0 / 3.0 * self.ux[i, -1] * self.rho[i, -1]
-                #self.f_in[i, -1, 5] = self.f_in[i, -1, 7] - 1.0 / 6.0 * self.ux[i, -1] * self.rho[i, -1]
-                #self.f_in[i, -1, 8] = self.f_in[i, -1, 6] - 1.0 / 6.0 * self.ux[i, -1] * self.rho[i, -1]
-
-                #self.f_in[i, -1, 3] = self.f_in[i, -1, 4] - 2.0 / 3.0 * self.ux[i, -1] * self.rho[i, -1]
-                #self.f_in[i, -1, 5] = self.f_in[i, -1, 7] - 1.0 / 6.0 * self.ux[i, -1] * self.rho[i, -1]
-                #self.f_in[i, -1, 8] = self.f_in[i, -1, 6] - 1.0 / 6.0 * self.ux[i, -1] * self.rho[i, -1]
-
-                #self.f_in[i, -1, :] = equilibrium(self.rho[i, -1], self.ux[i, -1], self.uy[i, -1], VELOCITIES, WEIGHTS)
-
-            # Lewa granica: liniowa zmiana \( u_x \) od dołu do góry, \( u_y = 0.0 \)
+            # Lewo: liniowa zmiana
             for i in range(self.height):
                 self.f_in[i, 0, :] = equilibrium(self.rho[i, 0], self.ux[i, 0], self.uy[i, 0], VELOCITIES, WEIGHTS)
 
             # Góra: symetryczny warunek
             for j in range(self.width):
-               # self.ux[0, :] = self.ux[1, :]
-               # self.f_in[0, j, :] = equilibrium(self.rho[0, j], self.ux[0, j], 0.0, VELOCITIES, WEIGHTS)
-
-               # Zdefiniowanie prędkości
-               #u_h = self.ux[1, j]  # Symetria z wierszem poniżej
-              # self.ux[0, j] = u_h  # Symetryczna prędkość
-              # self.uy[0, j] = 0.0  # Prędkość pionowa zerowa
-
-               # Wyliczanie gęstości z równania (16e)
-              # self.rho[0, j] = (self.f_in[0, j, 0] + 2 * (self.f_in[0, j, 1] + self.f_in[0, j, 3]) + 4 * (self.f_in[0, j, 5] + self.f_in[0, j, 7])) / (1 - u_h)
-
                self.f_in[0, j, 4] = self.f_in[0, j, 3]
                self.f_in[0, j, 8] = self.f_in[0, j, 5]
                self.f_in[0, j, 7] = self.f_in[0, j, 6]
 
-
-               #self.f_in[0, j, :] = equilibrium(self.rho[0, j], self.ux[0, j], 0.0, VELOCITIES, WEIGHTS)
-
-               # Aktualizacja funkcji rozkładu zgodnie z teorią
-             #  self.f_in[0, j, 2] = self.f_in[0, j, 4]  # \( f_{in}^{v} = f_{in}^{-v} \)
-         #      self.f_in[0, j, 1] = self.f_in[0, j, 3] + (2 / 3) * self.rho[0, j] * u_h  # \( f_{in}^{h} \)
-          #     self.f_in[0, j, 5] = self.f_in[0, j, 7] + (1 / 6) * self.rho[0, j] * u_h  # \( f_{in}^{vh} \)
-            #   self.f_in[0, j, 8] = self.f_in[0, j, 6] + (1 / 6) * self.rho[0, j] * u_h  # \( f_{in}^{-vh} \)
+               self.uy[0, j] = 0.0
 
             # Dół: bounce-back
-
-
             for j in range(self.width):
                 self.f_in[-1, j, 3] = self.f_in[-1, j, 4]
                 self.f_in[-1, j, 5] = self.f_in[-1, j, 7]
                 self.f_in[-1, j, 6] = self.f_in[-1, j, 8]
-
-                #for k in range(9):
-                #    self.f_in[-1, j, k] = self.f_out[-1, j, OPPOSITE_DIRECTIONS[k]]
 
     def compute_macroscopic(self):
         self.rho = np.sum(self.f_in, axis=2)
@@ -215,20 +164,6 @@ class LBM:
         self.ux[non_zero_mask] = np.sum(self.f_in * np.array([v[0] for v in VELOCITIES]), axis=2)[non_zero_mask] / self.rho[non_zero_mask]
         self.uy[non_zero_mask] = np.sum(self.f_in * np.array([v[1] for v in VELOCITIES]), axis=2)[non_zero_mask] / self.rho[non_zero_mask]
 
-        '''
-        # Narzucenie stałych wartości na granicach
-        self.ux[0, :] = 0.02
-        self.uy[0, :] = 0.0
-        self.ux[-1, :] = 0.0
-        self.uy[-1, :] = 0.0
-
-        for i in range(self.height):
-            self.ux[self.height - i - 1, 0] = 0.02 * (i / (self.height - 1))
-            self.ux[self.height - i - 1, -1] = 0.02 * (i / (self.height - 1))
-            self.uy[self.height - i - 1, 0] = 0.0
-            self.uy[self.height - i - 1, -1] = 0.0
-
-        '''
         # Ograniczenie prędkości do zakresu [-u_max, u_max]
         u_max = MAX_SPEED
         self.ux = np.clip(self.ux, -u_max, u_max)
